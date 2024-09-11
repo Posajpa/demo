b89460452502b38428c36f671cdcad5ecb0fa69c
Pooria Sajadi Parsa <131904326+Posajpa@users.noreply.github.com>
2024-09-11 15:17:58 +0200

    Update kafka_client.py

--- config/kafka_client.py
+++ config/kafka_client.py
@@ -1,10 +1,4 @@
 import socket
 
 # Confluent Cloud credentials
-conf = {'bootstrap.servers': 'pkc-57rr02.italynorth.azure.confluent.cloud:9092',
-        'security.protocol': 'SASL_SSL',
-        'sasl.mechanism': 'PLAIN',
-        'sasl.username': '6E6UXX22DIQLMPGL',
-        'sasl.password': 'u6QGaZ0UR88hjHHQosWJoHrb5qxYTF3u6Wz6RJOL+I8BSbt4yvpAErmjvAeO98Z5',
-        'client.id': socket.gethostname()
-  }
+conf = 
